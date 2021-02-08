97bcb38b91eb136dd56b812fe103105369f4c5fb
WonSik36 <cws0173@gmail.com>
2021-02-08 16:53:48 +0900

    설정 수정

--- src/main/java/rookie/sample/acount/config/ContainerConfig.java
+++ src/main/java/rookie/sample/acount/config/ContainerConfig.java
@@ -1,6 +1,7 @@
 package rookie.sample.acount.config;
 
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -18,25 +19,20 @@ public class ContainerConfig {
 
     @Bean
     public ServletWebServerFactory servletContainer() {
-
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-
         tomcat.addAdditionalTomcatConnectors(createAjpConnector());
 
         return tomcat;
-
     }
 
-
-
     private Connector createAjpConnector() {
-
         Connector ajpConnector = new Connector(ajpProtocol);
+        ajpConnector.setProperty("address","0.0.0.0");
+        ajpConnector.setProperty("allowedRequestAttributesPattern",".*");
+        ((AbstractAjpProtocol)ajpConnector.getProtocolHandler()).setSecretRequired(false);
 
         ajpConnector.setPort(ajpPort);
-
         ajpConnector.setSecure(false); ajpConnector.setAllowTrace(false);
-
         ajpConnector.setScheme("http");
 
         return ajpConnector;
