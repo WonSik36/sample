c74158a05f3e98f15714fee7ab42812049c2766f
WonSik36 <cws0173@gmail.com>
2021-02-08 16:12:45 +0900

    first commit

---
+++ src/main/java/rookie/sample/acount/SampleAcountApplication.java
@@ -0,0 +1,13 @@
+package rookie.sample.acount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAcountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAcountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rookie/sample/acount/SampleAcountApplicationTests.java
@@ -0,0 +1,13 @@
+package rookie.sample.acount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAcountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
